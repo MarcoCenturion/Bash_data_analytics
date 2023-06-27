477d9dcce621bb8e850c8db664078ea7b3396fba
MarcoCenturion <aulavirtualturismo@gmail.com>
2023-06-27 20:07:15 -0300

    bash scripting case_prueba con color rojo

--- case_prueba.sh
+++ case_prueba.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 #
 # Aprendiendo a usar sed, cat, awk, cut
 # (c) Marco Centurion
@@ -12,22 +12,26 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 blueColour="\e[0;34m\033[1m"
+redColour="\e[0;31m\033[1m"
+
+
 # Establecemos la fecha en la variable now
 now=$(date +"%d-%m-%y  %H:%M")
 
-function pantalla_ingreso(){
-clear
+function renglon(){
+        printf %100s |tr " " "="
+}
+
 # Ingresamos el valor del dolar y el Markup
 echo -ne "\n${greenColour}Ingresar el valor del dolarblue: ${endColour}" 
 read dolarblue
-echo -ne "\n${blueColour}------------------------------${endColour}"
+renglon
 echo -ne "\n${greenColour}Esta cotización es del día: $now${endColour}"
 echo -ne "\n${greenColour}El valor ingresado es: $dolarbue ${endColour}"
 echo -ne "\n${greenColour}Indicar el Markup por tkt: ${endColour}" 
 read markup
-}
+renglon
 
-function parseado_inter(){
 # Parseamos el documento txt en cada una de las variables que 
 # utilizaremos para hacer la cotización
 #
@@ -36,12 +40,9 @@ tarifa_usd=$(cat $1 | grep USD | awk 'NR == 1 {print $2}')
 equipaje=$(cat $1 | grep -E '2P|1P|0P'|awk '{print $10}')
 tramos=$(cat $1 | grep -e '^  [1-9]'|cut -c 6-12,15-20,23-29,35-43 | awk '{print $0}')
 tarifa=$(cat $1 | grep -e '^ARS' | tail -1 | awk '{print $1}' | sed 's/ARS//' | awk -F . '{print $1}')
-}
 
-function cotizacion_inter(){
 # Imprimimos en pantalla la cotización
-clear
-echo -e "\n${blueColour}\n----------------------------------${endColour}"
+renglon
 echo -en "\n${blueColour}\nFecha de Cotización Emitida: ${endColour}"
 echo -e "${redColour}$now${endColour}"
 echo -en "\n${blueColours}Ultimo día para emitir: ${endColour}"
@@ -56,43 +57,15 @@ echo -en "\n${blueColour}Tarifa en Pesos Total por pasajero: ARS ${endColour}"
 echo -e "${redColour}$(($tarifa+$markup))${endColour}"
 echo -en "\n${blueColour}Tarifa en Dolares total por pasajero: USD ${endColour}"
 echo -e "${redColour}$((($tarifa+$markup)/$dolarblue))${endColour}"
-}
 
-# -------------------------------
-# Case para elegir entre las 3 opciones, para implementar mas adelante
-# echo "Seleccionar opción"
-# echo "------------------"
-# echo "1) Cabotaje"
-# echo "2) Internacional originado en Argentina"
-# echo "3) Internacional originado en el exterior"
-# read seleccion # Elegir una de las opciones 
-# case $eleccion in 
-#	1 ) 
-#	echo "Cabotaje" ;;
-#	2 )
-#		pantalla_ingreso
-#		parseado_inter
-#		cotizacion_inter;;
-#	3 )
-#                pantalla_ingreso;;
-# esac
-
-if [[ -f $1 ]]  # Validamos que el usuario agregue un argumento, el archivo .txt con los datos del PNR
-then
-	echo "Leyendo el archivo "$1
-	sleep 1 
-
-#function ruta(){
-#cat $1 | grep 
-#}
+# if [[ -f $1 ]]  # Validamos que el usuario agregue un argumento, el archivo .txt con los datos del PNR
+# then
+#	echo "Leyendo el archivo "$1
+#	sleep 1 
 
 # Arrancamos las funciones
 
-pantalla_ingreso
-parseado_inter
-cotizacion_inter
+# else
+# 	echo "El archivo" $1 "no fue encontrado" # Mensaje cuando no pasamos el archivo como parámetro.
+# fi
 
-
-else
-	echo "El archivo" $1 "no fue encontrado" # Mensaje cuando no pasamos el archivo como parámetro.
-fi
