c22432e2c18681a445f079a90ef87cfd87c2ffd8
MarcoCenturion <aulavirtualturismo@gmail.com>
2023-06-28 23:12:57 -0300

    case_color con endosos

--- case_prueba.sh
+++ case_prueba.sh
@@ -40,23 +40,28 @@ tarifa_usd=$(cat $1 | grep USD | awk 'NR == 1 {print $2}')
 equipaje=$(cat $1 | grep -E '2P|1P|0P'|awk '{print $10}')
 tramos=$(cat $1 | grep -e '^  [1-9]'|cut -c 6-12,15-20,23-29,35-43 | awk '{print $0}')
 tarifa=$(cat $1 | grep -e '^ARS' | tail -1 | awk '{print $1}' | sed 's/ARS//' | awk -F . '{print $1}')
+endos=$(cat $1 | grep -e 'REF')
 
 # Imprimimos en pantalla la cotización
 renglon
 echo -en "\n${blueColour}\nFecha de Cotización Emitida: ${endColour}"
 echo -e "${redColour}$now${endColour}"
-echo -en "\n${blueColours}Ultimo día para emitir: ${endColour}"
+echo -en "\n${blueColour}Ultimo día para emitir: ${endColour}"
 echo -e "${redColour}$ltd${endColour}"
 echo -en "\n${blueColour}Importe en USD sin TAX:  ${endColour}"
 echo -e "${redColour}$tarifa_usd${endColour}"
 echo -e "\n${blueColour}Cía|Vuelo|Fecha|Ruta|Sale|Llega${endColour}"
 echo -e "${redColour}$tramos${endColour}"
-echo -en "\n${blueColour}Equipaje Permitido: ${endColour}"
+echo -en "\n${blueColour}Equipaje Permitido: ${endColour}\n"
 echo -e "${redColour}$equipaje${endColour}"
+echo -en "\n${blueColour}Devoluciones y cambios: ${endColour}\n"
+echo -e "${redColour}$endos${endColour}"
 echo -en "\n${blueColour}Tarifa en Pesos Total por pasajero: ARS ${endColour}"
 echo -e "${redColour}$(($tarifa+$markup))${endColour}"
 echo -en "\n${blueColour}Tarifa en Dolares total por pasajero: USD ${endColour}"
 echo -e "${redColour}$((($tarifa+$markup)/$dolarblue))${endColour}"
+renglon 
+exit 1
 
 # if [[ -f $1 ]]  # Validamos que el usuario agregue un argumento, el archivo .txt con los datos del PNR
 # then
